ba9dcb2ade71af1d3860924788ed1bee92235ee1
R. Quazi <112246222+qriz1452@users.noreply.github.com>
2023-12-09 11:35:10 +0530

    added hello world print function

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+def print_hello_world_banner():
+    banner = """
+    *****************************************
+    *                                       *
+    *           Hello, World!                *
+    *                                       *
+    *****************************************
+    """
+    print(banner)
+
+# Calling the function print_hello_world_banner to print the banner
+print_hello_world_banner()
+
+# printing  MapUp Assignment 2 
+print("\nThis is MapUp Assignment 2.")
